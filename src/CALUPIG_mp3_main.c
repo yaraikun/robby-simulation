403b88ad26dae9521a856439cf4f9e1b01d8fa75
yaraikun <evan.calupig@gmail.com>
2024-10-07 23:28:19 +0800

    Finished barebones switch case main menu. Screen still doesn't clear itself but that's fine for testing probably.

--- src/CALUPIG_mp3_main.c
+++ src/CALUPIG_mp3_main.c
@@ -23,11 +23,13 @@
 // Main function for Robby simulation.
 int main(void) {
 
+    // printf("\033[H\033[J");
+    
     // Declared six variables.
     float fRobotX, fRobotY;         // Robby's position (x, y).
     double dRobotAngle;             // Robby's orientation (in degrees).
     float fDistance;                // Robby's translation distance.
-    double dTheta;                  // 
+    double dTheta;                  // Robby's rotation angle.
     int nCommandCode;               // User command code.
 
     // Call the function InitializeReset() to initialize the robot's position
@@ -69,7 +71,7 @@ int main(void) {
                 break;
             case 4:
                 Quit();
-                break;
+                return 0;
             case 8:
                 printf("Enter translation distance: ");
                 scanf("%f", &fDistance);
@@ -80,46 +82,12 @@ int main(void) {
                 scanf("%lf", &dTheta);
                 RotateCounterClockwise(dTheta, &dRobotAngle);
                 break;
+            default:
+                printf("Invalid command code. Please Try again.\n");
+                break;
         }
     }
-  /* 
-     NOTE #1:
-     You are allowed to declare only SIX variables corresponding to:
-     - robot's position, i.e., x and y coordinates  (2 variables)
-     - robot's orientation (1 variable)
-     - translation distance (1 variable)
-     - rotation angle (1 variable)
-     - command code (1 variable)
-   
-     NOTE #2:
-     Write the C statements corresponding to the following steps:
-     1. First and foremost, call the function InitializeReset() to initialize
-        the robot's position and orientation.
-     2. Ask the user to input the command code.  If the command requires
-        additional data (for example: translate command requires a translation
-        distance, while the rotate command requires a rotation angle), 
-        ask the user to input the appropriate data value using scanf().
-     3. Call the function that corresponds to the command code.
-     4. If the command code is not yet for the Quit() function, repeat again
-        from step 2. If the command code is for quit, terminate the program
-        execution.
 
-    
-     NOTE #3: 
-     Make sure that you use the macro names which were defined in the 
-     mp3_robot.h file.  They can be used with if-else statement or with
-     switch-case statement.
-  
-   
-     NOTE #4: (VERY IMPORTANT!)
-     The main() function that you are implementing here is actually for your 
-     use;
-     i.e., so that you can check and verify the correctness of your solution.
-     For the actual check, I will use my own main() function and link it with 
-     your lastname_mp3_robot and lastname_mp2_math object files.  The resulting 
-     executable file should also be 100% semantically correct for you to earn a 
-     perfect grade.   
-  */
-
-  return 0;
+    // Program completed succesfully.
+    return 0;
 }
